aab6bfce7fcecfc8a1df43af221b7f933b4273ec
Mateusz Fijałkowski <fijalkowskim01@gmail.com>
2023-12-07 14:19:00 +0100

    Navbar hover animation

--- meetallmortys/src/components/Navbar/NavbarButton.tsx
+++ meetallmortys/src/components/Navbar/NavbarButton.tsx
@@ -1,11 +1,20 @@
 import React from "react";
+import { motion } from "framer-motion";
 interface Props {
   text: String;
   route: String;
 }
 function NavbarButton({ text, route }: Props) {
   return (
-    <a className="text-black text-2xl text-inherit cursor-pointer ">{text}</a>
+    <motion.a
+      animate={{ scale: 1 }}
+      whileHover={{ scale: 1.05 }}
+      whileTap={{ scale: 0.97 }}
+      className={`group text-gray-800 text-2xl cursor-pointer hover:text-black leading-tight active:text-red-800`}
+    >
+      {text}
+      <div className="h-[1px] w-full bg-gray-800 origin-center scale-0 group-hover:scale-100 transition-transform"></div>
+    </motion.a>
   );
 }
 
