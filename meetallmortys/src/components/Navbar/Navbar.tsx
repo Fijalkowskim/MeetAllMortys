d358fdcc8e3e45b36b8a04116ca2ec9903be83df
Mateusz Fijałkowski <fijalkowskim01@gmail.com>
2023-12-10 11:54:58 +0100

    Navbar buttons animation

--- meetallmortys/src/components/Navbar/Navbar.tsx
+++ meetallmortys/src/components/Navbar/Navbar.tsx
@@ -5,17 +5,11 @@ import NavbarButton from "./NavbarButton";
 function Navbar() {
   return (
     <nav className="fixed top-0 left-0 right-0 z-50 font-primary grid place-content-center w-[100vw]">
-      {/* <div className="absolute aspect-square bg-white rounded-full h-[16rem] translate-x-[-30%] translate-y-[-40%] shadow-md" />
-      <img
-        src={MeetAllMortysLogo}
-        alt="Meet All Mortys Logo"
-        className="absolute rounded-full max-w-none h-32"
-      /> */}
       <ul className="w-fit px-12 py-2 flex gap-24 items-center justify-center rounded-bl-full rounded-br-full standard-border border-t-0">
         <NavbarButton text={"Home"} route={"/"} />
-        <NavbarButton text={"Characters"} route={"/"} />
-        <NavbarButton text={"Places"} route={"/"} />
-        <NavbarButton text={"Epizodes"} route={"/"} />
+        <NavbarButton text={"Characters"} route={"/characters"} />
+        <NavbarButton text={"Places"} route={"/places"} />
+        <NavbarButton text={"Epizodes"} route={"/epizodes"} />
         <NavbarButton text={"About"} route={"/about"} />
       </ul>
     </nav>
--- meetallmortys/src/components/Navbar/NavbarButton.tsx
+++ meetallmortys/src/components/Navbar/NavbarButton.tsx
@@ -1,26 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { motion } from "framer-motion";
-import { NavLink, NavLinkProps } from "react-router-dom";
+import { NavLink, NavLinkProps, useLocation } from "react-router-dom";
 interface Props {
   text: String;
   route: String;
 }
 function NavbarButton({ text, route }: Props) {
+  const location = useLocation();
+  const [isActive, setIsActive] = useState(false);
+
+  useEffect(() => {
+    checkIfActive();
+  }, []);
+  const checkIfActive = () => {
+    setIsActive(location.pathname === route);
+  };
   return (
-    <NavLink
-      to={route as NavLinkProps["to"]}
-      className={"active:text-action-500"}
+    <motion.div
+      animate={{ scale: 1 }}
+      whileHover={{ scale: 1.05 }}
+      whileTap={{ scale: 1.05 }}
+      className="group cursor-pointer"
+      onClick={checkIfActive}
     >
-      <motion.a
-        animate={{ scale: 1 }}
-        whileHover={{ scale: 1.05 }}
-        whileTap={{ scale: 0.97 }}
-        className={`group text-gray-800 text-2xl cursor-pointer hover:text-black leading-tight active:text-action-500`}
-      >
-        {text}
-        <div className="h-[1px] w-full bg-gray-800 origin-center scale-0 group-hover:scale-100 transition-transform"></div>
-      </motion.a>
-    </NavLink>
+      <NavLink to={route as NavLinkProps["to"]}>
+        <motion.p
+          className={`group  text-2xl ${
+            isActive ? "text-action-500" : "text-gray-800 hover:text-black"
+          }  leading-tigh`}
+        >
+          {text}
+          <div
+            className={`h-[1px] w-full  ${
+              isActive
+                ? "bg-action-500 scale-100"
+                : " bg-gray-800 group-hover:scale-100"
+            }   origin-center scale-0  transition-transform`}
+          ></div>
+        </motion.p>
+      </NavLink>
+    </motion.div>
   );
 }
 
