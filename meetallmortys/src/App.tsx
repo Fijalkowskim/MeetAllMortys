2f7472d2dbb731ebd4d715245b4406469a94ed9a
Mateusz Fijałkowski <fijalkowskim01@gmail.com>
2023-12-10 11:38:07 +0100

    Navbar routing

--- meetallmortys/src/App.tsx
+++ meetallmortys/src/App.tsx
@@ -1,17 +1,20 @@
 import React from "react";
-
+import Navbar from "./components/Navbar/Navbar";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
-    </BrowserRouter>
+    <div className="relative">
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
--- meetallmortys/src/components/Navbar/NavbarButton.tsx
+++ meetallmortys/src/components/Navbar/NavbarButton.tsx
@@ -1,18 +1,21 @@
 import React from "react";
 import { motion } from "framer-motion";
-import { NavLink } from "react-router-dom";
+import { NavLink, NavLinkProps } from "react-router-dom";
 interface Props {
   text: String;
   route: String;
 }
 function NavbarButton({ text, route }: Props) {
   return (
-    <NavLink to={"/"} className={"active:text-action-500"}>
+    <NavLink
+      to={route as NavLinkProps["to"]}
+      className={"active:text-action-500"}
+    >
       <motion.a
         animate={{ scale: 1 }}
         whileHover={{ scale: 1.05 }}
         whileTap={{ scale: 0.97 }}
-        className={`group text-2xl cursor-pointer`}
+        className={`group text-gray-800 text-2xl cursor-pointer hover:text-black leading-tight active:text-action-500`}
       >
         {text}
         <div className="h-[1px] w-full bg-gray-800 origin-center scale-0 group-hover:scale-100 transition-transform"></div>
