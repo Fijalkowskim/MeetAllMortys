b1c17bd174985b74d0be31b45bdfe5f7327072c3
Mateusz Fijałkowski <fijalkowskim01@gmail.com>
2023-12-28 11:51:10 +0100

    Characters name display

--- meetallmortys/src/components/Characters/CharacterCard.tsx
+++ meetallmortys/src/components/Characters/CharacterCard.tsx
@@ -7,9 +7,10 @@ interface Props{
 }
 function CharacterCard({data}:Props) {
   return (
-    <motion.div className='aspect-square h-60 cursor-pointer'>
+    <motion.div className=' cursor-pointer flex flex-col items-center'>
         <img src={data.image} alt={data.name} 
-        className='object-cover object-center content-center rounded-full'/>
+        className='aspect-square h-60 object-cover object-center content-center rounded-full border-4 border-white'/>
+        <h1 className={`bg-white text-black px-4 rounded-xl text-center py-1 my-4 ${data.name.length >= 16 ? 'text-2xl' : 'text-4xl'}`}>{data.name}</h1>
         </motion.div>
   )
 }
