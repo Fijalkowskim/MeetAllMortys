6ab43e1afab809beff6650be545f79fda8061bec
Mateusz Fijałkowski <fijalkowskim01@gmail.com>
2023-12-28 11:53:30 +0100

    Simple image hover animaiton

--- meetallmortys/src/components/Characters/CharacterCard.tsx
+++ meetallmortys/src/components/Characters/CharacterCard.tsx
@@ -8,7 +8,9 @@ interface Props{
 function CharacterCard({data}:Props) {
   return (
     <motion.div className=' cursor-pointer flex flex-col items-center'>
-        <img src={data.image} alt={data.name} 
+        <motion.img src={data.image} alt={data.name} 
+        whileHover={{scale:1.05}}
+        transition={{type:"spring"}}
         className='aspect-square h-60 object-cover object-center content-center rounded-full border-4 border-white'/>
         <h1 className={`bg-white text-black px-4 rounded-xl text-center py-1 my-4 ${data.name.length >= 16 ? 'text-2xl' : 'text-4xl'}`}>{data.name}</h1>
         </motion.div>
