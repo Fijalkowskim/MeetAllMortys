9c760804f73fd3d1ada36f599ed9c72ff3af6cb6
Mateusz Fijałkowski <fijalkowskim01@gmail.com>
2023-12-29 21:14:35 +0100

    Character cards group hover

--- meetallmortys/src/components/Characters/CharacterCard.tsx
+++ meetallmortys/src/components/Characters/CharacterCard.tsx
@@ -7,11 +7,22 @@ interface Props {
 }
 function CharacterCard({ data }: Props) {
   return (
-    <motion.div className=" cursor-pointer flex flex-col items-center font-futuristic">
+    <motion.div
+      initial="initial"
+      whileHover="whileHover"
+      className=" cursor-pointer flex flex-col items-center font-futuristic"
+    >
       <motion.img
         src={data.image}
         alt={data.name}
-        whileHover={{ scale: 1.05 }}
+        variants={{
+          initial: {
+            scale: 1,
+          },
+          whileHover: {
+            scale: 1.05,
+          },
+        }}
         transition={{ type: "spring" }}
         className="aspect-square h-60 object-cover object-center content-center rounded-full border-4 border-white"
       />
