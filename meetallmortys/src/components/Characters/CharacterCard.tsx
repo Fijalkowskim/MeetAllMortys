30d11293f7947a18781fa5fc4b62646126691474
Mateusz Fijałkowski <fijalkowskim01@gmail.com>
2023-12-29 21:30:28 +0100

    Character card name hover animation popup

--- meetallmortys/src/components/Characters/CharacterCard.tsx
+++ meetallmortys/src/components/Characters/CharacterCard.tsx
@@ -8,35 +8,42 @@ interface Props {
 function CharacterCard({ data }: Props) {
   return (
     <motion.div
-      initial="initial"
+      animate="animate"
       whileHover="whileHover"
-      className=" cursor-pointer flex flex-col items-center font-futuristic"
+      className=" cursor-pointer flex flex-col items-center font-futuristic relative mb-20"
     >
       <motion.img
         src={data.image}
         alt={data.name}
         variants={{
-          initial: {
+          animate: {
             scale: 1,
+            borderRadius: 200,
           },
           whileHover: {
             scale: 1.05,
+            borderRadius: 100,
           },
         }}
         transition={{ type: "spring" }}
-        className="aspect-square h-60 object-cover object-center content-center rounded-full border-4 border-white"
+        className="aspect-square h-70 object-cover object-center content-center border-4 border-white z-10"
       />
       <motion.h1
         className={`bg-white text-black px-4 rounded-xl text-center py-1 my-4 ${
           data.name.length >= 16 ? "text-2xl" : "text-4xl"
         }`}
         variants={{
-          initial: {
-            scale: 1,
+          animate: {
+            opacity: 0,
           },
           whileHover: {
-            scale: 1.05,
+            opacity: 1,
+            y: 150,
           },
+        }}
+        style={{
+          position: "absolute",
+          top: "50%",
         }}
         transition={{ type: "spring" }}
       >
--- meetallmortys/src/components/Characters/CharactersDisplay.tsx
+++ meetallmortys/src/components/Characters/CharactersDisplay.tsx
@@ -1,20 +1,20 @@
-import React, {useEffect} from 'react'
-import { useCharactersContext } from '../../context/CharactersContext';
-import CharacterCard from './CharacterCard';
-import {motion} from 'framer-motion'
+import React, { useEffect } from "react";
+import { useCharactersContext } from "../../context/CharactersContext";
+import CharacterCard from "./CharacterCard";
+import { motion } from "framer-motion";
 
 function CharactersDisplay() {
-    const {characters,loadCharacters} = useCharactersContext();
-    useEffect(()=>{
-        loadCharacters(1);
-    },[])
-    return (
-        <motion.div className='flex flex-wrap gap-6 w-full py-20 px-6 mx-auto justify-center cursor pointer'>
-          {characters?.map((char) => (
-            <CharacterCard key={char.id} data={char}/>
-          ))}
-        </motion.div>
-      );
+  const { characters, loadCharacters } = useCharactersContext();
+  useEffect(() => {
+    loadCharacters(1);
+  }, []);
+  return (
+    <motion.div className="flex flex-wrap gap-x-6 w-full py-20 px-6 mx-auto justify-center cursor pointer">
+      {characters?.map((char) => (
+        <CharacterCard key={char.id} data={char} />
+      ))}
+    </motion.div>
+  );
 }
 
-export default CharactersDisplay
+export default CharactersDisplay;
