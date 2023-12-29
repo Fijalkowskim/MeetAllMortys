91ae6c63200bc2809c65996ecfa3062f733b2540
Mateusz Fijałkowski <fijalkowskim01@gmail.com>
2023-12-29 21:15:20 +0100

    Character name hover animation

--- meetallmortys/src/components/Characters/CharacterCard.tsx
+++ meetallmortys/src/components/Characters/CharacterCard.tsx
@@ -26,13 +26,22 @@ function CharacterCard({ data }: Props) {
         transition={{ type: "spring" }}
         className="aspect-square h-60 object-cover object-center content-center rounded-full border-4 border-white"
       />
-      <h1
+      <motion.h1
         className={`bg-white text-black px-4 rounded-xl text-center py-1 my-4 ${
           data.name.length >= 16 ? "text-2xl" : "text-4xl"
         }`}
+        variants={{
+          initial: {
+            scale: 1,
+          },
+          whileHover: {
+            scale: 1.05,
+          },
+        }}
+        transition={{ type: "spring" }}
       >
         {data.name}
-      </h1>
+      </motion.h1>
     </motion.div>
   );
 }
