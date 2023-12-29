3b7627f22b819aba47ef55c02ce06f1f9661a148
Mateusz Fijałkowski <fijalkowskim01@gmail.com>
2023-12-29 19:15:04 +0100

    Character card new font

--- meetallmortys/src/components/Characters/CharacterCard.tsx
+++ meetallmortys/src/components/Characters/CharacterCard.tsx
@@ -1,20 +1,29 @@
-import React from 'react'
-import { CharacterData } from '../../model/CharacterData'
-import {motion} from 'framer-motion'
+import React from "react";
+import { CharacterData } from "../../model/CharacterData";
+import { motion } from "framer-motion";
 
-interface Props{
- data: CharacterData
+interface Props {
+  data: CharacterData;
 }
-function CharacterCard({data}:Props) {
+function CharacterCard({ data }: Props) {
   return (
-    <motion.div className=' cursor-pointer flex flex-col items-center'>
-        <motion.img src={data.image} alt={data.name} 
-        whileHover={{scale:1.05}}
-        transition={{type:"spring"}}
-        className='aspect-square h-60 object-cover object-center content-center rounded-full border-4 border-white'/>
-        <h1 className={`bg-white text-black px-4 rounded-xl text-center py-1 my-4 ${data.name.length >= 16 ? 'text-2xl' : 'text-4xl'}`}>{data.name}</h1>
-        </motion.div>
-  )
+    <motion.div className=" cursor-pointer flex flex-col items-center font-futuristic">
+      <motion.img
+        src={data.image}
+        alt={data.name}
+        whileHover={{ scale: 1.05 }}
+        transition={{ type: "spring" }}
+        className="aspect-square h-60 object-cover object-center content-center rounded-full border-4 border-white"
+      />
+      <h1
+        className={`bg-white text-black px-4 rounded-xl text-center py-1 my-4 ${
+          data.name.length >= 16 ? "text-2xl" : "text-4xl"
+        }`}
+      >
+        {data.name}
+      </h1>
+    </motion.div>
+  );
 }
 
-export default CharacterCard
+export default CharacterCard;
