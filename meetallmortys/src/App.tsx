b739544b8b041334027fd6e9348948905ee9d3be
Mateusz Fijałkowski <fijalkowskim01@gmail.com>
2024-01-01 20:12:39 +0100

    Routing to characters with page argument

--- meetallmortys/src/App.tsx
+++ meetallmortys/src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Navbar from "./components/Navbar/Navbar";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Characters from "./pages/Characters";
@@ -13,7 +13,9 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
-          <Route path="/characters" element={<Characters />} />
+          <Route path="/characters" element={<Navigate to="/characters/1" />} />
+          <Route path="/characters/:page" element={<Characters />} />
+          <Route path="*" element={<Navigate to="/" />} />
         </Routes>
       </BrowserRouter>
     </div>
